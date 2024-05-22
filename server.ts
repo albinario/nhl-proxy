87caab89c4708c206decdee69e128b947c632d1c
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-05-22 16:46:47 +0200

    update server start message

--- server.ts
+++ server.ts
@@ -44,5 +44,5 @@ server.on('error', (err: NodeJS.ErrnoException) => {
  * Event listener for HTTP server "listening" event.
  */
 server.on('listening', () => {
-	console.log(`💥 Yay, server started on http://localhost:${PORT}`)
+	console.log(`💥 Yay, server started on port ${PORT}`)
 })
